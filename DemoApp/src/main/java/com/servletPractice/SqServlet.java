a2b4df5fae47ad1bcf914def25134159a3658be3
Prasad Rao <prasad1027@gmail.com>
2023-11-08 05:06:59 -0600

    Initial comit.November 8th 2024. Got the servlet working with tomcat server of my Dynamic web project.

---
+++ DemoApp/src/main/java/com/servletPractice/SqServlet.java
@@ -0,0 +1,16 @@
+package com.servletPractice;
+import java.io.IOException;
+
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+
+
+public class SqServlet extends  {
+	public void doGet(HttpServletRequest req,HttpServletResponse res  ) throws IOException
+	{
+		
+	}
+
+}
